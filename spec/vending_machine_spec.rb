00ffaebec16b732162e372cf46288fc686c48daa
leyb-opinya <daniel@opinya.co.il>
2020-10-20 16:47:51 +0300

    refactor

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,41 +1,46 @@
 # frozen_string_literal: true
 
+require_relative 'spec_helper'
 require_relative '../vending_machine'
 
 describe VendingMachine do
-  it 'coke with 4.75' do
-    expect(
-      subject.buy_product('coke', [2,2,0.5,0.25])
-    ).to eq({:message=>"Here's your change: \n1 of $1\n1 of $0.5\n1 of $0.25", :status=>:success})
+  context 'success flow' do
+    it 'coke with 4.75' do
+      expect(
+        subject.buy_product('coke', [2, 2, 0.5, 0.25])
+      ).to eq(message: "Here's your change: \n1 of $1\n1 of $0.5\n1 of $0.25", status: :success)
+    end
+
+    it 'coke with 4.25' do
+      expect(
+        subject.buy_product('coke', [2, 1, 1, 0.5, 0.5, 0.25])
+      ).to eq(message: "Here's your change: \n1 of $2\n1 of $0.25", status: :success)
+    end
+
+    it 'coke with 5.25' do
+      expect(
+        subject.buy_product('coke', [5, 0.25])
+      ).to eq(message: "Here's your change: \n1 of $2\n1 of $0.25", status: :success)
+    end
+
+    it 'coke with 3 - exact value' do
+      expect(
+        subject.buy_product('coke', [2, 1])
+      ).to eq(message: '', status: :success)
+    end
   end
 
-  it 'coke with 4.25' do
-    expect(
-      subject.buy_product('coke', [2,1,1,0.5,0.5,0.25])
-    ).to eq({:message=>"Here's your change: \n1 of $2\n1 of $0.25", :status=>:success})
-  end
+  context 'failure cases' do
+    it 'coke with 1' do
+      expect(
+        subject.buy_product('coke', [1])
+      ).to eq(message: 'not enough money', status: :failed)
+    end
 
-  it 'coke with 5.25' do
-    expect(
-      subject.buy_product('coke', [5, 0.25])
-    ).to eq({:message=>"Here's your change: \n1 of $2\n1 of $0.25", :status=>:success})
-  end
-
-  it 'coke with 3 - exact value' do
-    expect(
-      subject.buy_product('coke', [2, 1])
-    ).to eq({:message=>"", :status=>:success})
-  end
-
-  it 'coke with 1' do
-    expect(
-      subject.buy_product('coke', [1])
-    ).to eq(message: 'not enough money', status: :failed)
-  end
-
-  it 'coke with 3.333' do
-    expect(
-      subject.buy_product('coke', [3.333])
-    ).to eq(message: 'unable to provide correct change', status: :failed)
+    it 'coke with 3.333' do
+      expect(
+        subject.buy_product('coke', [3.333])
+      ).to eq(message: 'unable to provide correct change', status: :failed)
+    end
   end
 end
---
+++ vending_machine_ui.rb
@@ -0,0 +1,41 @@
+class VendingMachineUI
+  attr_accessor :money_in_progress
+
+  def initialize
+    @console = CLI::Console.new(HighLine.new)
+    @vending_machine = VendingMachine.new
+    @money_in_progress = []
+  end
+
+  def offer_items
+    puts "\n"
+    puts 'Choose which product you want or exit to leave'.blue.on_light_black
+    item_chosen = @console.getCommand(@vending_machine.products.join("\n"))
+    exit if item_chosen == 'exit'
+    return puts 'Product N/A'.black.on_white unless @vending_machine.product(item_chosen)
+
+    puts 'Please insert money in one of these formats: '
+    ask_for_more_money(item_chosen)
+    @money_in_progress = []
+  end
+
+  private
+
+  def ask_for_more_money(item_chosen)
+    money_inserted = @console.getCommand(@vending_machine.coins_accepted.join(', ')).to_f
+    if @vending_machine.coins_accepted.none? money_inserted
+      puts 'Coin unknown'.red
+      # FIXME: balance is lost
+      return
+    end
+    @money_in_progress.push(money_inserted)
+    if @vending_machine.enough_money_to_buy_product?(item_chosen, @money_in_progress.sum)
+      result = @vending_machine.buy_product(item_chosen, @money_in_progress)
+      puts result[:message].blue.on_white
+      result[:status]
+    else
+      puts "Not enough balance to buy product (#{@money_in_progress.sum}). Please add more"
+      ask_for_more_money(item_chosen)
+    end
+  end
+end
